e93c31dc7194366fb1163f9b0b3483a8bcb6b1a2
gugunm <mediamergugun2@gmail.com>
2021-11-09 11:36:58 +0700

    change api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://apisimhpnas.bpkp.go.id'
+export const API_URL = 'https://apisimhpnas.bpkp.go.id'
 // export const API_URL = 'http://10.10.2.45'
 // export const API_URL = 'http://10.10.20.43:8001'
 // export const API_URL = 'http://simhpnas.bpkp.go.id:8001'
